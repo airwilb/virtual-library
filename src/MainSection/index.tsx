4c933438f4ae25de103a17e38b000313c519de31
Nate Ben <netanelben@users.noreply.github.com>
2024-05-10 18:01:11 +0300

    refactor audio element

--- src/MainSection/index.tsx
+++ src/MainSection/index.tsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useRef } from "react";
 import MainSectionImage from "./main-section-bg.png";
 import { Tombstone } from "../Tombstone";
 import { useBookList } from "../api";
 import "./style.css";
 
 export const MainSection = () => {
+  const audioRef = useRef();
   const books = useBookList();
 
   return (
@@ -17,9 +18,16 @@ export const MainSection = () => {
       <div className="in-front-bat-graphics main-screen" />
       <div className="tombstones-container">
         {books.map((book, key) => (
-          <Tombstone {...book} key={key} index={key} />
+          <Tombstone {...book} key={key} index={key} audioRef={audioRef} />
         ))}
       </div>
+      <audio
+        ref={audioRef}
+        src="https://media.graphassets.com/2vcSLJYJRiMcT2gPO42E?_gl=1*1v4roi8*_ga*MTA1MzIzNjc2My4xNzA0MDQxMjk2*_ga_G6FYGSYGZ4*MTcwNTc1MTEzOC4xMi4xLjE3MDU3NTI4NTguNDguMC4w"
+        controls={false}
+        autoPlay={false}
+        loop={false}
+      />
     </div>
   );
 };
--- src/Tombstone/index.tsx
+++ src/Tombstone/index.tsx
@@ -1,23 +1,22 @@
 import React from "react";
 import Modal from "react-modal";
 import Tomb01 from "./tombtone_v2.png";
-import "./style.css";
-import { useRef } from "react";
 import { ModalContent } from "./ModalContent";
 import { modalCustomStyles } from "../config";
 import { Book } from "../api";
+import "./style.css";
 
 export const Tombstone = (
   props: {
     index: number;
+    audioRef: React.RefObject<HTMLAudioElement>;
   } & Book
 ) => {
   const [modalIsOpen, setIsOpen] = React.useState(false);
-  const audioRef = useRef();
 
   function openModal() {
     setIsOpen(true);
-    audioRef.current && audioRef.current.play();
+    props.audioRef.current && props.audioRef.current.play();
   }
 
   function closeModal() {
@@ -26,13 +25,6 @@ export const Tombstone = (
 
   return (
     <>
-      <audio
-        ref={audioRef}
-        src="https://media.graphassets.com/2vcSLJYJRiMcT2gPO42E?_gl=1*1v4roi8*_ga*MTA1MzIzNjc2My4xNzA0MDQxMjk2*_ga_G6FYGSYGZ4*MTcwNTc1MTEzOC4xMi4xLjE3MDU3NTI4NTguNDguMC4w"
-        controls={false}
-        autoPlay={false}
-        loop={false}
-      />
       <div
         className="tombstone-wrapper"
         onClick={openModal}
